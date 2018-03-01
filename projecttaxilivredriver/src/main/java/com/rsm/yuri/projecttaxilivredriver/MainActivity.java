7ef96533f250f8ae69faf49b523e62e4a87a13e6
Yuri07 <yuri_resende@hotmail.com>
2018-02-28 22:42:54 -0300

    Dependencias, bibliotecas e chaves de API adicionadas, Layout básico da MainActivity (sem os fragments) implementado.

--- projecttaxilivredriver/src/main/java/com/rsm/yuri/projecttaxilivredriver/MainActivity.java
+++ projecttaxilivredriver/src/main/java/com/rsm/yuri/projecttaxilivredriver/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rsm.yuri.projecttaxilivredriver;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
